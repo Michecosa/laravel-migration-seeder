cf06d7e74e2055f79bd20e6eeb705dd9d16edc51
Michela <micheladellagatta1@gmail.com>
2026-02-11 22:05:18 +0100

    feat: filter and sort trains by departure date

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Train;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PageController extends Controller
 {
     public function index() {
-        $trains = Train::all();
+        $trains = Train::where('departure_time', '>=', Carbon::today())
+        ->orderBy('departure_time', 'asc')
+        ->get();
 
         return view('index', compact('trains'));
     }
