a42836db71cc751b08e5d9c62dd1bff789242608
Michela <micheladellagatta1@gmail.com>
2026-02-12 12:13:18 +0100

    feat: implement TrainsSeeder with Faker for automatic data population

---
+++ database/seeders/TrainsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+class TrainsSeeder extends Seeder
+{
+    public function run(Faker $faker): void
+    {
+        for ($i=0; $i < 50; $i++) { 
+            $newTrain = new Train();
+
+            $newTrain->agency = $faker-> company();
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->train_code = $faker->bothify('??#####');
+            $newTrain->number_of_carriages = $faker->numberBetween(3, 15);
+            $newTrain->is_on_time = $faker->boolean();
+            $newTrain->is_cancelled = $faker->boolean(10);
+
+            $newTrain->save();
+        }
+    }
+}
